1e69c7cac103b92e9df68ce7d48837b86e5f56cf
Farhad Ali Irinel Gul <farhadgul2001@gmail.com>
2024-06-10 13:48:34 +0300

    Added board files

---
+++ software/board_files/esp32-c6-sparrow/pins_arduino.h
@@ -0,0 +1,60 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include "soc/soc_caps.h"
+#include <stdint.h>
+
+static const uint8_t PIN_NEOPIXEL = 3;
+
+// UART
+static const uint8_t TX = 16;
+static const uint8_t RX = 17;
+
+// I2C
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
+
+// SPI
+static const uint8_t SS = 10;
+static const uint8_t SD_CS = 10;
+static const uint8_t FLASH_CS = 23;
+static const uint8_t MOSI = 7;
+static const uint8_t MISO = 2;
+static const uint8_t SCK = 6;
+
+// ANALOG
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
+static const uint8_t A6 = 6;
+
+// GPIO
+static const uint8_t GPIO0 = 0;
+static const uint8_t GPIO1 = 1;
+static const uint8_t GPIO2 = 2;
+static const uint8_t GPIO3 = 3;
+static const uint8_t GPIO4 = 4;
+static const uint8_t GPIO5 = 5;
+static const uint8_t GPIO6 = 6;
+static const uint8_t GPIO7 = 7;
+static const uint8_t GPIO8 = 8;
+static const uint8_t GPIO9 = 9;
+static const uint8_t GPIO10 = 10;
+static const uint8_t GPIO11 = 11;
+static const uint8_t GPIO12 = 12;
+static const uint8_t GPIO13 = 13;
+static const uint8_t GPIO14 = 14;
+static const uint8_t GPIO15 = 15;
+static const uint8_t GPIO16 = 16;
+static const uint8_t GPIO17 = 17;
+static const uint8_t GPIO18 = 18;
+static const uint8_t GPIO19 = 19;
+static const uint8_t GPIO20 = 20;
+static const uint8_t GPIO21 = 21;
+static const uint8_t GPIO22 = 22;
+static const uint8_t GPIO23 = 23;
+
+#endif /* Pins_Arduino_h */
